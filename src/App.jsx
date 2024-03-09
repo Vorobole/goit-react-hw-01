6a6f2e5792a62f94a6aeb2731a8c66d8497b8b6e
Vorobole <vorobyov1903@gmail.com>
2024-03-09 16:42:15 +0200

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import Profile from "./components/Profile/Profile";
+import Profile from "./components/Profile/Profile.jsx";
 import userData from "./data/userData.json";
-import FriendList from "./components/FriendList/FriendList";
+import FriendList from "./components/FriendList/FriendList.jsx";
 import friends from "./data/friends.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
 import transactions from "./data/transaction.json";
 
 function App() {
